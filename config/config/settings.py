9d6d14417e023fc4024ef388bd563ad490832be7
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-08-04 21:19:56 +0900

    migration

--- config/config/settings.py
+++ config/config/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
